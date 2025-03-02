69a5c3ed48cc9a279601663cfb12fc39042c3220
Mritunjay Patel <97620757+Mrpatel2319@users.noreply.github.com>
2025-03-02 13:52:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const weatherData = {
+    temperature: 25,
+    conditions: "Sunny"
+  };
+
+  
+  const temperatureColor = {
+    color: weatherData.temperature > 20 ? 'rgb(255, 0, 0)' : 'blue'
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>Temperature: <span style={temperatureColor}>{weatherData.temperature}</span></p>
+      <p>Conditions: {weatherData.conditions}</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
